5f1405552dad104263f1065aa3dcf3c52d85f1f8
novacbn <31122674+novacbn@users.noreply.github.com>
2021-10-27 21:51:01 -0400

    - Added button styling support to `AppAnchor`

--- packages/@kahi-docs-web/src/components/AppAnchor.svelte
+++ packages/@kahi-docs-web/src/components/AppAnchor.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
     import {page} from "$app/stores";
-    import type {PROPERTY_PALETTE} from "@kahi-ui/framework";
+    import type {
+        PROPERTY_PALETTE,
+        PROPERTY_SIZING,
+        PROPERTY_VARIATION_BUTTON,
+    } from "@kahi-ui/framework";
 
     import {is_current_page, is_external_url} from "@kahi-docs/shared";
+
+    export let is: "a" | "button" = "a";
 
     export let href: string;
 
@@ -10,18 +16,37 @@
     export let prefetch: boolean = false;
 
     export let palette: PROPERTY_PALETTE | undefined = undefined;
+    export let variation: PROPERTY_VARIATION_BUTTON | undefined = undefined;
+    export let size: PROPERTY_SIZING | undefined = undefined;
 
     $: _is_external = is_external_url(href);
 </script>
 
-<a
-    aria-current={is_current_page($page, href) ? "page" : undefined}
-    {href}
-    target={_is_external ? "_blank" : undefined}
-    rel={_is_external ? "noopener noreferrer" : undefined}
-    sveltekit:noscroll={no_scroll ? true : undefined}
-    sveltekit:prefetch={prefetch ? true : undefined}
-    data-palette={palette}
->
-    <slot />
-</a>
+{#if is === "button"}
+    <a
+        class="button"
+        aria-current={is_current_page($page, href) ? "page" : undefined}
+        {href}
+        target={_is_external ? "_blank" : undefined}
+        rel={_is_external ? "noopener noreferrer" : undefined}
+        sveltekit:noscroll={no_scroll ? true : undefined}
+        sveltekit:prefetch={prefetch ? true : undefined}
+        data-palette={palette}
+        data-size={size}
+        data-variation={variation}
+    >
+        <slot />
+    </a>
+{:else}
+    <a
+        aria-current={is_current_page($page, href) ? "page" : undefined}
+        {href}
+        target={_is_external ? "_blank" : undefined}
+        rel={_is_external ? "noopener noreferrer" : undefined}
+        sveltekit:noscroll={no_scroll ? true : undefined}
+        sveltekit:prefetch={prefetch ? true : undefined}
+        data-palette={palette}
+    >
+        <slot />
+    </a>
+{/if}
