af3f032c76ad0c9ad7f8b49c5cdbf67547d86e59
novacbn <31122674+novacbn@users.noreply.github.com>
2021-10-29 13:50:40 -0400

    - Added first batch to Lucide icons

---
+++ packages/@kahi-docs-web/src/components/icons/ArrowRight.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    /**
+     *  https://github.com/lucide-icons/lucide
+     *
+     *  ISC License
+     *
+     *  Copyright (c) 2020, Lucide Contributors
+     *
+     *  Permission to use, copy, modify, and/or distribute this software for any
+     *  purpose with or without fee is hereby granted, provided that the above
+     *  copyright notice and this permission notice appear in all copies.
+     *
+     *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+     *  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+     *  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+     *  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+     *  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+     *  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+     *  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+     */
+
+    export let size: string = "1em";
+</script>
+
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+>
+    <line x1="5" y1="12" x2="19" y2="12" />
+    <polyline points="12 5 19 12 12 19" />
+</svg>
---
+++ packages/@kahi-docs-web/src/components/icons/MessageCircle.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    /**
+     *  https://github.com/lucide-icons/lucide
+     *
+     *  ISC License
+     *
+     *  Copyright (c) 2020, Lucide Contributors
+     *
+     *  Permission to use, copy, modify, and/or distribute this software for any
+     *  purpose with or without fee is hereby granted, provided that the above
+     *  copyright notice and this permission notice appear in all copies.
+     *
+     *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+     *  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+     *  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+     *  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+     *  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+     *  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+     *  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+     */
+
+    export let size: string = "1em";
+</script>
+
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+>
+    <path
+        d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"
+    />
+</svg>
