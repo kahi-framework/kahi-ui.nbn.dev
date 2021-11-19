19471a95c7f8ba7f247c5fb12d4e8059034bde76
novacbn <31122674+novacbn@users.noreply.github.com>
2021-11-19 15:16:56 -0500

    - Added keyboard arrow navigation to search

--- packages/@kahi-docs-web/src/lib/client/keybind.ts
+++ packages/@kahi-docs-web/src/lib/client/keybind.ts
@@ -1,5 +1,11 @@
 import type {IKeybindCallback} from "@kahi-ui/framework";
 import {keybind} from "@kahi-ui/framework";
 
+export const next_keybind = (element: HTMLElement, on_bind: IKeybindCallback) =>
+    keybind(element, {on_bind, binds: "arrowdown", repeat: true, repeat_throttle: 250});
+
+export const previous_keybind = (element: HTMLElement, on_bind: IKeybindCallback) =>
+    keybind(element, {on_bind, binds: "arrowup", repeat: true, repeat_throttle: 250});
+
 export const search_keybind = (element: HTMLElement, on_bind: IKeybindCallback) =>
     keybind(element, {on_bind, binds: "control+/", repeat: true});
