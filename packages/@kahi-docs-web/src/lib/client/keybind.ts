86bb3bad3fd0b444b3a2bf2f654d07259ee21f73
novacbn <31122674+novacbn@users.noreply.github.com>
2021-11-19 15:47:10 -0500

    - Fixed typing for keybind shortcuts

--- packages/@kahi-docs-web/src/lib/client/keybind.ts
+++ packages/@kahi-docs-web/src/lib/client/keybind.ts
@@ -1,11 +1,11 @@
-import type {IKeybindCallback} from "@kahi-ui/framework";
+import type {IKeybindCallback, IKeybindHandle} from "@kahi-ui/framework";
 import {keybind} from "@kahi-ui/framework";
 
-export const next_keybind = (element: HTMLElement, on_bind: IKeybindCallback) =>
+export const next_keybind = (element: HTMLElement, on_bind: IKeybindCallback): IKeybindHandle =>
     keybind(element, {on_bind, binds: "arrowdown", repeat: true, repeat_throttle: 250});
 
-export const previous_keybind = (element: HTMLElement, on_bind: IKeybindCallback) =>
+export const previous_keybind = (element: HTMLElement, on_bind: IKeybindCallback): IKeybindHandle =>
     keybind(element, {on_bind, binds: "arrowup", repeat: true, repeat_throttle: 250});
 
-export const search_keybind = (element: HTMLElement, on_bind: IKeybindCallback) =>
+export const search_keybind = (element: HTMLElement, on_bind: IKeybindCallback): IKeybindHandle =>
     keybind(element, {on_bind, binds: "control+/", repeat: true});
