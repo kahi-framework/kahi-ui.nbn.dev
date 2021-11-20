c936c71b25046fbb48876e1d0e8360c6cca5014a
novacbn <31122674+novacbn@users.noreply.github.com>
2021-11-19 19:26:47 -0500

    - Adjusted navigation link ordering

--- .kahi-docs/navigation.config.ts
+++ .kahi-docs/navigation.config.ts
@@ -145,9 +145,9 @@ export default define_navigation({
             text: "Overlays",
             items: [
                 {href: "/docs/overlays/clickable"},
-                {href: "/docs/overlays/popover", badge: "UPDATED"},
                 {href: "/docs/overlays/offscreen", badge: "UPDATED"},
                 {href: "/docs/overlays/overlay", badge: "UPDATED"},
+                {href: "/docs/overlays/popover", badge: "UPDATED"},
             ],
         },
 
