94ce910084015ed571b1aece9c61dc6e4940b821
novacbn <31122674+novacbn@users.noreply.github.com>
2021-11-26 21:42:21 -0500

    - Added initial batch of controls to Playground

--- packages/@kahi-docs-web/src/routes/playground/index.svelte
+++ packages/@kahi-docs-web/src/routes/playground/index.svelte
@@ -41,26 +41,96 @@
 <script lang="ts">
     import {browser} from "$app/env";
 
+    import {Box, Menu, Spacer, Stack, Text} from "@kahi-ui/framework";
+
     import type {ISnippet} from "@kahi-docs/markdown";
 
     import {session} from "@kahi-docs/shared";
 
     import HeroJavascriptEnabled from "../../lib/components/HeroJavascriptEnabled.svelte";
+    import {SPLIT_MODE, SPLIT_ORIENTATION} from "../../lib/components/Split.svelte";
     import StaticLayout from "../../lib/components/StaticLayout.svelte";
+
+    import Copy from "../../lib/components/icons/Copy.svelte";
+    import Code from "../../lib/components/icons/Code.svelte";
+    import Image from "../../lib/components/icons/Image.svelte";
+    import RotateCW from "../../lib/components/icons/RotateCW.svelte";
+    import Sidebar from "../../lib/components/icons/Sidebar.svelte";
 
     import REPLSplit from "../../lib/components/repl/REPLSplit.svelte";
 
     export let script: string | undefined;
     export let snippet: ISnippet | undefined;
 
+    let mode: keyof typeof SPLIT_MODE = SPLIT_MODE.split;
+    let orientation: keyof typeof SPLIT_ORIENTATION = SPLIT_ORIENTATION.horizontal;
     let value: string = snippet?.script ?? script ?? ($session || "");
+
+    function on_copy_click(event: MouseEvent): void {
+        navigator.clipboard.writeText(value);
+    }
 
     $: if (browser) $session = value;
 </script>
 
 {#if browser}
     <StaticLayout>
-        <REPLSplit bind:value />
+        <Box padding="small">
+            <Stack orientation="horizontal">
+                <Spacer />
+                <Menu.Container orientation="horizontal" sizing="small">
+                    <Menu.Button palette="affirmative" variation="clear" on:click={on_copy_click}>
+                        <Copy />
+                        Copy
+                    </Menu.Button>
+
+                    <Menu.Button
+                        palette="inverse"
+                        variation="clear"
+                        on:click={() =>
+                            (orientation =
+                                orientation === SPLIT_ORIENTATION.horizontal
+                                    ? SPLIT_ORIENTATION.vertical
+                                    : SPLIT_ORIENTATION.horizontal)}
+                    >
+                        <RotateCW />
+                        <Text is="span" hidden="mobile">Rotate</Text>
+                    </Menu.Button>
+
+                    <Menu.Button
+                        active={mode === SPLIT_MODE.split}
+                        palette="inverse"
+                        variation="clear"
+                        on:click={() => (mode = SPLIT_MODE.split)}
+                    >
+                        <Sidebar />
+                        <Text is="span" hidden="mobile">Split</Text>
+                    </Menu.Button>
+
+                    <Menu.Button
+                        active={mode === SPLIT_MODE.first}
+                        palette="inverse"
+                        variation="clear"
+                        on:click={() => (mode = SPLIT_MODE.first)}
+                    >
+                        <Code />
+                        <Text is="span" hidden="mobile">Editor</Text>
+                    </Menu.Button>
+
+                    <Menu.Button
+                        active={mode === SPLIT_MODE.last}
+                        palette="inverse"
+                        variation="clear"
+                        on:click={() => (mode = SPLIT_MODE.last)}
+                    >
+                        <Image />
+                        <Text is="span" hidden="mobile">Render</Text>
+                    </Menu.Button>
+                </Menu.Container>
+            </Stack>
+        </Box>
+
+        <REPLSplit {mode} {orientation} bind:value />
     </StaticLayout>
 {:else}
     <HeroJavascriptEnabled />
