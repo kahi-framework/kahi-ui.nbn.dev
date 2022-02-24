f933bdf85af82ff694f3057756611518e6d663f5
novacbn <31122674+novacbn@users.noreply.github.com>
2022-02-23 22:25:10 -0500

    - First pass at cleaning up Markdown render output styling

--- packages/@kahi-docs-web/src/lib/components/ContentArticle.svelte
+++ packages/@kahi-docs-web/src/lib/components/ContentArticle.svelte
@@ -1,45 +1,30 @@
-<article class="container content-article" data-viewport="desktop" data-padding-bottom="large">
+<article
+    class="container article content-article"
+    data-max-width="desktop"
+    data-width="100"
+    data-padding-y="large"
+>
     <slot />
 </article>
 
 <style>
-    :global(.content-article) :global(a:not([data-palette])) {
-        color: rgb(var(--palette-accent-boldest));
-    }
-
-    :global(.content-article) :global(.repl-render) :global(a:not([data-palette])) {
-        color: inherit;
-    }
-
-    :global(.content-article) > :global(h1),
-    :global(.content-article) > :global(h2),
-    :global(.content-article) > :global(h3),
     :global(.content-article) > :global(.tab) :global(h3),
-    :global(.content-article) > :global(h4),
-    :global(.content-article) > :global(.tab) :global(h4),
-    :global(.content-article) > :global(h5),
-    :global(.content-article) > :global(h6) {
+    :global(.content-article) > :global(.tab) :global(h4) {
         margin-top: var(--spacing-local-medium);
         margin-bottom: 0.25em;
     }
 
-    :global(.content-article) > :global(blockquote),
-    :global(.content-article) > :global(p) {
-        margin-top: var(--spacing-local-medium);
+    :global(.content-article) > :global(pre[class*="language-"]) {
+        margin: 0;
+    }
+
+    :global(.content-article) > :global(pre[class*="language-"]:not(:last-child)),
+    :global(.content-article) > :global(.repl-snippet:not(:last-child)) {
+        margin-bottom: calc(var(--code-article-margin-y) * 1rem);
     }
 
     :global(.content-article) > :global(.scrollable),
-    :global(.content-article) > :global(ul),
     :global(.content-article) > :global(.tab) {
         margin-top: var(--spacing-root-medium);
-    }
-
-    :global(.content-article) > :global(ul) {
-        margin-left: var(--spacing-root-large);
-    }
-
-    :global(.content-article) > :global(.highlight),
-    :global(.content-article) > :global(.repl-snippet) {
-        margin: var(--spacing-root-medium) 0 0 0;
     }
 </style>
