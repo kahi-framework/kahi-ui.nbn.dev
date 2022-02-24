9c34e22bca75b7180192d8c3d907c28a3c6918ac
novacbn <31122674+novacbn@users.noreply.github.com>
2022-02-24 00:01:26 -0500

    - Removed dead stylings - Fixed margins for scrolling tables

--- packages/@kahi-docs-web/src/lib/components/ContentArticle.svelte
+++ packages/@kahi-docs-web/src/lib/components/ContentArticle.svelte
@@ -8,12 +8,6 @@
 </article>
 
 <style>
-    :global(.content-article) > :global(.tab) :global(h3),
-    :global(.content-article) > :global(.tab) :global(h4) {
-        margin-top: var(--spacing-local-medium);
-        margin-bottom: 0.25em;
-    }
-
     :global(.content-article) > :global(pre[class*="language-"]) {
         margin: 0;
     }
@@ -23,8 +17,7 @@
         margin-bottom: calc(var(--code-article-margin-y) * 1rem);
     }
 
-    :global(.content-article) > :global(.scrollable),
-    :global(.content-article) > :global(.tab) {
-        margin-top: var(--spacing-root-medium);
+    :global(.content-article) > :global(.scrollable:not(:last-child)) {
+        margin-bottom: calc(var(--spacings-block-medium) * 1rem);
     }
 </style>
