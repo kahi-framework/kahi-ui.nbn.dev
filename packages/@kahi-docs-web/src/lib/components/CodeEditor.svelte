ffb3941a0593587c05f0d4eff7c95b09b04cf1ef
novacbn <31122674+novacbn@users.noreply.github.com>
2022-03-04 07:28:03 -0500

    - Updated REPL components to use a more simplified workflow for importing the runtime imports - Updated REPL components to cache their imports - Added debouncing of render updates for better performance

---
+++ packages/@kahi-docs-web/src/lib/components/CodeEditor.svelte
@@ -0,0 +1,23 @@
+<script context="module" lang="ts">
+    import {highlight as docs_highlight} from "@kahi-docs/highlight";
+
+    const highlight = (text: string) => docs_highlight(text, "svelte");
+</script>
+
+<script lang="ts">
+    import {CodeJar} from "@novacbn/svelte-codejar";
+
+    export let value: string;
+</script>
+
+<CodeJar class="code-editor" syntax="svelte" {highlight} bind:value />
+
+<style>
+    :global(.code-editor) {
+        margin: 0 !important;
+
+        height: 100%;
+
+        white-space: pre !important;
+    }
+</style>
